31a78c3daff4d3dad711a35b653824b25f72a700
“emanuelbesliu” <emanuelbesliuph@gmail.com>
2020-10-03 13:15:11 +0300

    refactoring

--- run.pl
+++ run.pl
@@ -1,4 +1,5 @@
 #!/usr/bin/perl -w
+
 sub slurp {
     open F,"<",shift;
     binmode F;
@@ -11,6 +12,3 @@ $y = slurp shift;
 use List::Util qw(min);
 vec($z, $_, 8) = vec($x, $_, 8) ^ vec($y, $_, 8) for (0 .. min(length $x, length $y) - 1);
 print $z;
-
-
-
